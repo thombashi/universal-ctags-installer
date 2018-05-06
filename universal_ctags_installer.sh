8f596551bc917232dfb8d0462ff15cd435f5dfc7
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-05-06 21:59:17 +0900

    Update the build script

--- universal_ctags_installer.sh
+++ universal_ctags_installer.sh
@@ -1,19 +1,25 @@
 #!/usr/bin/env sh
 
-set -eu
+set -e
 
-if [ -e /etc/debian_version ] || [ -e /etc/debian_release ]; then
-    apt install autoconf pkg-config -y
-elif [ -e /etc/fedora-release ] || [ -e /etc/redhat-release ]; then
-    dnf install autoconf -y
-fi
-
-work_dir="$(mktemp --directory)"
+dist=$1
+curr_dir=$(pwd)
+work_dir=$(mktemp --directory)
 cd "$work_dir"
+
 git clone https://github.com/universal-ctags/ctags.git
 cd ctags
+
 ./autogen.sh
-./configure
-make
+
+if [ "$dist" = "" ]; then
+    ./configure
+else
+    ./configure --prefix="$dist"
+fi
+
+make -j2
 make install
+
+cd "$curr_dir"
 rm -rf "$work_dir"
